a79c33e43e61aee5e603025521c83800a81084a9
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-23 12:55:01 +0300

    Update income component details

--- src/components/Income.tsx
+++ src/components/Income.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 
+const today = new Date().toISOString().split("T")[0];
+
 const Income = () => {
   return (
     <div>
       <p className="h2">Income</p>
+      
       <label htmlFor="income-source" className="form-label mt-5">
         Income source
       </label>
       <input type="text" className="form-control" id="income-source" />
-      <label htmlFor="income-source" className="form-label mt-3">
+      
+      <label htmlFor="amount-source" className="form-label mt-3">
         Amount of income
       </label>
-      <input type="number" className="form-control" id="income-source" />
+      <div className="input-group mb-3">
+        <span className="input-group-text">$</span>
+        <input
+          type="number"
+          id="amount-source"
+          className="form-control"
+          aria-label="Amount (to the nearest dollar)"
+          min={1}
+          defaultValue={1}
+        />
+        <span className="input-group-text">.00</span>
+      </div>
 
       <div className="mt-3 mb-5">
         <label htmlFor="dateInput" className="form-label">
           Date of income
         </label>
-        <input type="date" className="form-control" id="dateInput" />
+        <input
+          type="date"
+          className="form-control"
+          id="dateInput"
+          defaultValue={today}
+        />
       </div>
 
       <button type="submit" className="btn btn-primary">
