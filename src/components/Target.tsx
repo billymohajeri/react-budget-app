a206c6c69793212f31d704ae86716883668a4782
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-23 13:33:47 +0300

    Update target component

--- src/components/Target.tsx
+++ src/components/Target.tsx
@@ -4,6 +4,26 @@ const Target = () => {
   return (
     <>
       <p className="h2">Target</p>
+
+      <label htmlFor="amount-expense" className="form-label mt-5">
+        Set target
+      </label>
+      <div className="input-group mb-3">
+        <span className="input-group-text">$</span>
+        <input
+          type="number"
+          id="amount-expense"
+          className="form-control"
+          aria-label="Amount (to the nearest dollar)"
+          min={1}
+          defaultValue={1}
+        />
+        <span className="input-group-text">.00</span>
+      </div>
+
+      <button type="submit" className="btn btn-outline-danger">
+        Reset
+      </button>
     </>
   );
 };
