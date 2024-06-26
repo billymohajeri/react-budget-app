ed2c14e6e904dd739c0760fcd5b8f0d2a1b2a4a9
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-26 17:29:51 +0300

    Update target component

--- src/components/Target.tsx
+++ src/components/Target.tsx
@@ -1,42 +1,54 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 const Target = () => {
+  const [target, setTarget] = useState(0);
+
+  const handleTargetChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTarget(Number(event.target.value));
+  };
+
+  const handleIncomeSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setTarget(0);
+  };
+
   return (
     <>
       <p className="h2">Target</p>
+      <form onSubmit={handleIncomeSubmit}>
+        <label htmlFor="targetAmount" className="form-label mt-5">
+          Set target
+        </label>
+        <div className="input-group mb-3">
+          <span className="input-group-text">€</span>
+          <input
+            type="number"
+            id="targetAmount"
+            name="targetAmount"
+            value={target}
+            className="form-control"
+            aria-label="Amount (to the nearest euro)"
+            min={0}
+            required
+            onChange={handleTargetChange}
+          />
+          <span className="input-group-text">.00</span>
+        </div>
 
-      <label htmlFor="amount-target" className="form-label mt-5">
-        Set target
-      </label>
-      <div className="input-group">
-        <span className="input-group-text">€</span>
-        <input
-          type="number"
-          id="amount-target"
-          className="form-control"
-          aria-label="Amount (to the nearest euro)"
-          min={0}
-        />
-        <span className="input-group-text">.00</span>
-      </div>
-
-      <button type="submit" className="btn btn-outline-danger mt-2">
-        Reset
-      </button>
-      <p className="mt-2">
-        Current saving:
-      </p>
-      <p className="mt-2">
-        Target:
-      </p>
-      <p className="mt-0">
-        Progress:
-      </p>
+        <button type="submit" className="btn btn-outline-danger mt-2">
+          Reset
+        </button>
+      </form>
+      <p className="mt-5">Current saving:</p>
+      <p className="mt-2">Target: {target}</p>
+      <p className="mt-0">Progress:</p>
       <div
         className="progress"
         role="progressbar"
         aria-label="Example with label"
-        aria-valuenow={25}
+        aria-valuenow={100}
         aria-valuemin={0}
-        aria-valuemax={100}
+        aria-valuemax={target}
       >
         <div
           className="progress-bar progress-bar-striped progress-bar-animated"
